11904a20d1871d43aad51de7c1149510bc56e0c7
Laci Szűcs <ifrozen@gmail.com>
2014-11-10 15:24:07 +0100

    Revert "add post data user hash check"
    This reverts commit 55d778ee6068044fbcb5b3a191927d3c0ce0b32f.

--- metrics/transform.js
+++ metrics/transform.js
@@ -43,19 +43,12 @@ function transform(options) {
   }
 
   var req = options.req;
-  var id;
-  if (req.body.hash !== undefined) {
-    id = req.body.hash;
-  }
-  else{
-    id = req.user ? req.user.hash : req.cookies.guest;
-  }
   return Object.freeze({
     level: options.level,
     ip: req.ip,
     url: req.originalUrl,
     message: options.message,
-    distinctId: id,
+    distinctId: req.user ? req.user.hash : req.cookies.guest,
     eventType: options.eventType,
     params: flattenObject(options.params),
     features: req.fflip.features,
