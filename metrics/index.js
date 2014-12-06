78829ad48f43feda51977baa4c8ab3f1db020dfd
Laci Szűcs <ifrozen@gmail.com>
2014-12-06 22:53:46 +0100

    timeTrackerFactory

--- metrics/index.js
+++ metrics/index.js
@@ -6,41 +6,47 @@ var stream = require('stream');
 var loggerStreamFactory = require('./loggerStream');
 var createLogger = require('./logger').createLogger;
 var transform = require('./transform');
+var timeTrackerFactory = require('./timeTrackerFactory');
+
+var LOG_LEVELS = ['info', 'debug', 'warning', 'error'];
 
 module.exports = function(transportConf) {
-  return {
-    createTracker: function createTracker(type, req, ban) {
-      req = req && req.user ? req : {user: {}, fflip: {}, cookies: {}};
-      ban = ban || {};
-      if (req.fflip.setForUser) {
-        req.fflip.setForUser({
-          hash: req.user && req.user.hash || req.cookies.guest
-        });
-      }
-      var passThrough = new stream.PassThrough({objectMode: true});
-      var loggerStream = loggerStreamFactory(createLogger, transportConf, type);
-
-      if (!ban.log) {
-        passThrough.pipe(loggerStream);
-      }
-
-      return _.reduce([
-        'info',
-        'debug',
-        'warning',
-        'error'
-      ], function(acc, elem) {
-        acc[elem] = function(eventName, params) {
-          passThrough.write(transform({
-            level: elem,
-            req: req,
-            message: eventName,
-            eventType: type,
-            params: params
-          }));
-        };
-        return acc;
-      }, {});
+  function createTracker(type, req, ban) {
+    req = req && req.user ? req : {user: {}, fflip: {}, cookies: {}};
+    ban = ban || {};
+    if (req.fflip.setForUser) {
+      req.fflip.setForUser({
+        hash: req.user && req.user.hash || req.cookies.guest
+      });
     }
+    var passThrough = new stream.PassThrough({objectMode: true});
+    var loggerStream = loggerStreamFactory(createLogger, transportConf, type);
+
+    if (!ban.log) {
+      passThrough.pipe(loggerStream);
+    }
+
+    return _.reduce(LOG_LEVELS, function(acc, elem) {
+      acc[elem] = function(eventName, params) {
+        passThrough.write(transform({
+          level: elem,
+          req: req,
+          message: eventName,
+          eventType: type,
+          params: params
+        }));
+      };
+      return acc;
+    }, {});
+  }
+
+  function createTimeTracker() {
+    return timeTrackerFactory(
+        createTracker.apply(null, Array.prototype.slice.call(arguments)));
+  }
+
+  return {
+    createTracker: createTracker,
+    createTimeTracker: createTimeTracker
   };
 };
---
+++ metrics/timeTrackerFactory.js
@@ -0,0 +1,83 @@
+'use strict';
+var _ = require('lodash');
+
+module.exports = function timeTrackerFactory(tracker) {
+  var timers = [];
+  var lastAt;
+
+  function time(key) {
+    var t = {
+      key: key,
+      hash: Math.random().toString(36).split('.')[1],
+      startedAt: new Date(),
+      laps: {}
+    };
+    _.assign(t, {
+      value: _.partial(value, t),
+      lap: _.partial(lap, t),
+      end: _.partial(timeEnd, t)
+    });
+    timers[key] = timers[key] || [];
+    timers[key][t.hash] = t;
+    return t;
+  }
+
+//  var value = (t) => _.omit(t, (v, k) => typeof v === 'function' || k === 'laps');
+  function value(t) {
+    return _.omit(t, function(v, k) {
+      return typeof v === 'function' || k === 'laps';
+    });
+  }
+
+  function lap(t, prefix) {
+    var now = new Date;
+    lastAt = now;
+    var lap = {
+      lapCount: t.laps.length,
+      prefix: prefix,
+      lapAt: now,
+      stepDuration: now - lastAt,
+      totalDuration: now - t.startedAt
+    };
+    t.laps[prefix] = lap;
+    return t;
+  }
+
+  function timeEndAll(key) {
+    return _.map(t[key], timeEnd);
+  }
+
+  function timeEnd(t) {
+    var now = new Date();
+    lastAt = now;
+    _.assign(t, {
+      endedAt: now.toString(),
+      duration: now - t.startedAt,
+      startedAt: t.startedAt.toString()
+    });
+
+    _.assign(t, _.mapValues(tracker, function(log) {
+      return function(msg, payload) {
+        log(msg||t.key, _.assign(payload || {}, value(t)));
+      };
+    }));
+
+    _(t.laps).each(function(lap) {
+      var lapInfo = {};
+      lapInfo[lap.prefix + 'At'] = lap.lapAt.toString();
+      lapInfo[lap.prefix + 'StepDuration'] = lap.stepDuration;
+      lapInfo[lap.prefix + 'TotalDuration'] = lap.totalDuration;
+      _.assign(t, lapInfo);
+    });
+
+    timers[t.key][t.hash] = undefined;
+    return t;
+  }
+
+  return {
+    time: time,
+    lap: lap,
+    timeEnd: timeEnd,
+    timeEndAll: timeEndAll
+  };
+};
